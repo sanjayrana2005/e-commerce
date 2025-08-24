799b0e54c94bebdfa2878c4c17bd2439a51d8f89
Sanjyrana <Sanjayrana5113@gmail.com>
2025-08-24 22:38:53 +0530

    aome changes in order page

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import NoData from '../components/NoData'
+import DisplayPriceInRupee from '../utils/DisplayPriceInRupee'
 
 const MyOrders = () => {
   const orders = useSelector(state => state.orders.order)
-  console.log("Orders", orders);
+ console.log(`Orders`, orders[0]);
 
   return (
     <section>
@@ -17,14 +18,18 @@ const MyOrders = () => {
         }
         {
           orders.map((order, index) => {
+             
             return (
               <div key={order._id + index + "order"} className='order rounded p-3 text-sm border mt-3'>
-                <p><span className='font-medium mr-2'>Order No :</span> {order?.orderId}</p>
+                <p><span className='font-medium mr-2'>Order No :</span>{order?.orderId}</p>
                 <div className='flex gap-3'>
                   <img src={order.product_details.image[0]} alt={order.product_details.name + "image"}
                     className='w-14 h-14 rounded'
                   />
-                  <p className='font-normal'>{order.product_details.name}</p>
+                  <div>
+                    <p className='font-normal'>{order.product_details.name}</p>
+                  <p className='bg-green-200 w-fit px-2 py-1 rounded'>{DisplayPriceInRupee(order.totalAmt)}</p>
+                  </div>
                 </div>
               </div>
             )
